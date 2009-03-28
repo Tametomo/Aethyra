065129339c4cf63316b97000560d4bd178fdaf4b
Ira Rice <irarice@gmail.com>
2009-03-28 14:13:58 -0600

    Modified autobuild.sh to be up-to-date. Apparently it got reverted back to an older version when testing the update.sh script.
    Signed-off-by: Ira Rice <irarice@gmail.com>

--- autobuild.sh
+++ autobuild.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # auto build script based on fedora but usable for other distros
 #author=Blame <blame582@gmail.com> 
-#last modified date 2008-09-29 Blameu
-# right now thes should be ok for testing 
+#last modified date 2009-03-28 Tametomo
+# right now this should be ok for testing 
 file=/etc/fedora-release
 file1=/etc/lsb-release
 
@@ -10,7 +10,7 @@ file1=/etc/lsb-release
 if [ -e $file1 ];then
         echo "Checking to ensure that required packages are installed"
         echo
-        sudo apt-get install build-essential automake autoconf
+        sudo apt-get install build-essential automake autoconf libtool cvs
         sudo apt-get install autoconf gettext git git-core g++
         sudo apt-get install libsdl1.2-dev libsdl-image1.2-dev libsdl-net1.2-dev libsdl-mixer1.2-dev 
         sudo apt-get install libphysfs-dev libcurl4-openssl-dev libgl1-mesa-dev 
