3e1f7713949bc56206de0f6bfb366d61211ba8f1
Ira Rice <irarice@gmail.com>
2009-03-28 12:41:35 -0600

    Added package checking to the auto building script for Ubuntu, to ensure that the build script won't fail using it.
    Signed-off-by: Ira Rice <irarice@gmail.com>

--- autobuild.sh
+++ autobuild.sh
@@ -6,6 +6,19 @@
 file=/etc/fedora-release
 file1=/etc/lsb-release
 
+# Ensures that all of the required files for building are present
+if [ -e $file1 ];then
+        echo "Checking to ensure that required packages are installed"
+        echo
+        sudo apt-get install build-essential automake autoconf libtool
+        sudo apt-get install autoconf gettext git git-core g++
+        sudo apt-get install libsdl1.2-dev libsdl-image1.2-dev libsdl-net1.2-dev libsdl-mixer1.2-dev 
+        sudo apt-get install libphysfs-dev libcurl4-openssl-dev libgl1-mesa-dev 
+        sudo apt-get install libglu1-mesa-dev liballegro4.2-dev libxml2-dev
+        sudo apt-get install libguichan-0.8.1-1 libguichan-sdl-0.8.1-1 libguichan-dev
+        sudo apt-get install libsdl-ttf2.0-0 libsdl-ttf2.0-dev
+fi
+
 echo "Generating build information using aclocal, autoheader, automake and autoconf."
 echo
 
