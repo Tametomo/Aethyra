6baa3943987fd9aa1a4ba8f34787079845635336
Bjørn Lindeijer <bjorn@lindeijer.nl>
2009-04-02 15:35:59 +0200

    Don't use assertions in MessageIn
    Not nice if some data corruption would cause the client to crash
    unnecessarily.

--- src/net/messagein.cpp
+++ src/net/messagein.cpp
@@ -20,7 +20,6 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#include <cassert>
 #include <SDL.h>
 #include <SDL_endian.h>
 
@@ -39,117 +38,128 @@ MessageIn::MessageIn(const char *data, unsigned int length):
     mId = readInt16();
 }
 
-Sint8 MessageIn::readInt8()
+int MessageIn::readInt8()
 {
-    assert(mPos < mLength);
-    return mData[mPos++];
+    int value = -1;
+    if (mPos < mLength)
+    {
+        value = (unsigned char) mData[mPos];
+    }
+    mPos += 1;
+    return value;
 }
 
-Sint16 MessageIn::readInt16()
+int MessageIn::readInt16()
 {
-    assert(mPos + 2 <= mLength);
+    int value = -1;
+    if (mPos + 2 <= mLength)
+    {
+#if SDL_BYTEORDER == SDL_BIG_ENDIAN
+        value = SDL_Swap16(*(Sint16*)(mData + mPos));
+#else
+        value = (*(Sint16*)(mData + mPos));
+#endif
+    }
     mPos += 2;
-#if SDL_BYTEORDER == SDL_BIG_ENDIAN
-    return SDL_Swap16(*(Sint16*)(mData + (mPos - 2)));
-#else
-    return (*(Sint16*)(mData + (mPos - 2)));
-#endif
+    return value;
 }
 
-Sint32 MessageIn::readInt32()
+int MessageIn::readInt32()
 {
-    assert(mPos + 4 <= mLength);
-    mPos += 4;
+    int value = -1;
+    if (mPos + 4 <= mLength)
+    {
 #if SDL_BYTEORDER == SDL_BIG_ENDIAN
-    return SDL_Swap32(*(Sint32*)(mData + (mPos - 4)));
+        value = SDL_Swap32(*(Sint32*)(mData + mPos));
 #else
-    return (*(Sint32*)(mData + (mPos - 4)));
+        value = (*(Sint32*)(mData + mPos));
 #endif
+    }
+    mPos += 4;
+    return value;
 }
 
 void MessageIn::readCoordinates(Uint16 &x, Uint16 &y, Uint8 &direction)
 {
-    assert(mPos + 3 <= mLength);
-
-    const char *data = mData + mPos;
-    Sint16 temp;
-
-    temp = MAKEWORD(data[1] & 0x00c0, data[0] & 0x00ff);
-    x = temp >> 6;
-    temp = MAKEWORD(data[2] & 0x00f0, data[1] & 0x003f);
-    y = temp >> 4;
-
-    direction = data[2] & 0x000f;
-
-    // Translate from eAthena format
-    switch (direction)
+    if (mPos + 3 <= mLength)
     {
-        case 0:
-            direction = 1;
-            break;
-        case 1:
-            direction = 3;
-            break;
-        case 2:
-            direction = 2;
-            break;
-        case 3:
-            direction = 6;
-            break;
-        case 4:
-            direction = 4;
-            break;
-        case 5:
-            direction = 12;
-            break;
-        case 6:
-            direction = 8;
-            break;
-        case 7:
-            direction = 9;
-            break;
-        default:
-            // OOPSIE! Impossible or unknown
-            direction = 0;
-    }
+        const char *data = mData + mPos;
+        Sint16 temp;
 
+        temp = MAKEWORD(data[1] & 0x00c0, data[0] & 0x00ff);
+        x = temp >> 6;
+        temp = MAKEWORD(data[2] & 0x00f0, data[1] & 0x003f);
+        y = temp >> 4;
+
+        direction = data[2] & 0x000f;
+
+        // Translate from eAthena format
+        switch (direction)
+        {
+            case 0:
+                direction = 1;
+                break;
+            case 1:
+                direction = 3;
+                break;
+            case 2:
+                direction = 2;
+                break;
+            case 3:
+                direction = 6;
+                break;
+            case 4:
+                direction = 4;
+                break;
+            case 5:
+                direction = 12;
+                break;
+            case 6:
+                direction = 8;
+                break;
+            case 7:
+                direction = 9;
+                break;
+            default:
+                // OOPSIE! Impossible or unknown
+                direction = 0;
+        }
+    }
     mPos += 3;
 }
 
 void MessageIn::readCoordinatePair(Uint16 &srcX, Uint16 &srcY,
-                              Uint16 &dstX, Uint16 &dstY)
+                                   Uint16 &dstX, Uint16 &dstY)
 {
-    assert(mPos + 5 <= mLength);
+    if (mPos + 5 <= mLength)
+    {
+        const char *data = mData + mPos;
+        Sint16 temp;
 
-    const char *data = mData + mPos;
-    Sint16 temp;
+        temp = MAKEWORD(data[3], data[2] & 0x000f);
+        dstX = temp >> 2;
 
-    temp = MAKEWORD(data[3], data[2] & 0x000f);
-    dstX = temp >> 2;
+        dstY = MAKEWORD(data[4], data[3] & 0x0003);
 
-    dstY = MAKEWORD(data[4], data[3] & 0x0003);
+        temp = MAKEWORD(data[1], data[0]);
+        srcX = temp >> 6;
 
-    temp = MAKEWORD(data[1], data[0]);
-    srcX = temp >> 6;
-
-    temp = MAKEWORD(data[2], data[1] & 0x003f);
-    srcY = temp >> 4;
-
+        temp = MAKEWORD(data[2], data[1] & 0x003f);
+        srcY = temp >> 4;
+    }
     mPos += 5;
 }
 
 void MessageIn::skip(unsigned int length)
 {
-    assert(mPos + length <= mLength);
     mPos += length;
 }
 
 std::string MessageIn::readString(int length)
 {
     // Get string length
-    if (length < 0) {
+    if (length < 0)
         length = readInt16();
-    }
 
     // Make sure the string isn't erroneous
     if (length < 0 || mPos + length > mLength) {
@@ -164,22 +174,4 @@ std::string MessageIn::readString(int length)
                            stringEnd ? stringEnd - stringBeg : length);
     mPos += length;
     return readString;
-}
-
-Sint8& operator<<(Sint8 &lhs, MessageIn &msg)
-{
-    lhs = msg.readInt8();
-    return lhs;
-}
-
-Sint16& operator<<(Sint16 &lhs, MessageIn &msg)
-{
-    lhs = msg.readInt16();
-    return lhs;
-}
-
-Sint32& operator<<(Sint32 &lhs, MessageIn &msg)
-{
-    lhs = msg.readInt32();
-    return lhs;
 }
