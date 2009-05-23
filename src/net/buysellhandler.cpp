mana/mana

Fixed a few strings for better translation
Just a few notes that would help us translators immensly:
  * Please do not split one sentence into several strings
  * Please try to refrain from inserting extra spaces into the strings

src/net/ea/buysellhandler.cpp
@@ -108,7 +108,7 @@ void BuySellHandler::handleMessage(MessageIn &msg)
             }
             else
             {
-                localChatTab->chatLog(_("Nothing to sell"), BY_SERVER);
+                localChatTab->chatLog(_("Nothing to sell."), BY_SERVER);
                 current_npc = 0;
             }
             break;
@@ -116,22 +116,22 @@ void BuySellHandler::handleMessage(MessageIn &msg)
         case SMSG_NPC_BUY_RESPONSE:
             if (msg.readInt8() == 0)
             {
-                localChatTab->chatLog(_("Thanks for buying"), BY_SERVER);
+                localChatTab->chatLog(_("Thanks for buying."), BY_SERVER);
             }
             else
             {
                 // Reset player money since buy dialog already assumed purchase
                 // would go fine
                 buyDialog->setMoney(player_node->getMoney());
-                localChatTab->chatLog(_("Unable to buy"), BY_SERVER);
+                localChatTab->chatLog(_("Unable to buy."), BY_SERVER);
             }
             break;
 
         case SMSG_NPC_SELL_RESPONSE:
             if (msg.readInt8() == 0)
-                localChatTab->chatLog(_("Thanks for selling"), BY_SERVER);
+                localChatTab->chatLog(_("Thanks for selling."), BY_SERVER);
             else
-                localChatTab->chatLog(_("Unable to sell"), BY_SERVER);
+                localChatTab->chatLog(_("Unable to sell."), BY_SERVER);
 
             break;
     }

Tametomo/Aethyra

Signed-off-by: Tametomo <irarice@gmail.com>

src/net/buysellhandler.cpp
@@ -108,7 +108,7 @@ void BuySellHandler::handleMessage(MessageIn *msg)
             }
             else
             {
-                chatWindow->chatLog(_("Nothing to sell"), BY_SERVER);
+                chatWindow->chatLog(_("Nothing to sell."), BY_SERVER);
                 current_npc = 0;
             }
             break;
@@ -116,23 +116,22 @@ void BuySellHandler::handleMessage(MessageIn *msg)
         case SMSG_NPC_BUY_RESPONSE:
             if (msg->readInt8() == 0)
             {
-                chatWindow->chatLog(_("Thanks for buying"), BY_SERVER);
+                chatWindow->chatLog(_("Thanks for buying."), BY_SERVER);
             }
             else
             {
                 // Reset player money since buy dialog already assumed purchase
                 // would go fine
                 buyDialog->setMoney(player_node->mGp);
-                chatWindow->chatLog(_("Unable to buy"), BY_SERVER);
+                chatWindow->chatLog(_("Unable to buy."), BY_SERVER);
             }
             break;
 
         case SMSG_NPC_SELL_RESPONSE:
             if (msg->readInt8() == 0)
-                chatWindow->chatLog(_("Thanks for selling"), BY_SERVER);
+                chatWindow->chatLog(_("Thanks for selling."), BY_SERVER);
             else
-                chatWindow->chatLog(_("Unable to sell"), BY_SERVER);
-
+                chatWindow->chatLog(_("Unable to sell."), BY_SERVER);
             break;
     }
 }