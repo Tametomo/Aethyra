a086986020e62167b7779d55e39492114b9d06d2
Tametomo <irarice@gmail.com>
2009-06-24 08:46:46 -0600

    Release modal mouse focus in DropDowns when focus is lost. If not released, this can cause the user to think that the client has frozen up when dialogs such as the quit dialog take modal focus.
    Signed-off-by: Tametomo <irarice@gmail.com>

--- src/bindings/guichan/widgets/dropdown.cpp
+++ src/bindings/guichan/widgets/dropdown.cpp
@@ -249,6 +249,12 @@ void DropDown::keyPressed(gcn::KeyEvent& keyEvent)
     }
 }
 
+void DropDown::focusLost(const gcn::Event& event)
+{
+    gcn::DropDown::focusLost(event);
+    releaseModalMouseInputFocus();
+}
+
 void DropDown::mouseWheelMovedUp(gcn::MouseEvent& mouseEvent)
 {
     if (!mDroppedDown)
