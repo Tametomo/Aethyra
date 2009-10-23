ce01f8aa8f1721fdcba3ebad5778b5edc5c3070d
Tametomo <irarice@gmail.com>
2009-10-23 14:26:04 -0600

    Fixed a crash with emotes that occurred when there is no emote loaded in an emote shortcut slot.
    Signed-off-by: Tametomo <irarice@gmail.com>

--- src/eathena/handlers/emoteshortcut.h
+++ src/eathena/handlers/emoteshortcut.h
@@ -44,7 +44,7 @@ class EmoteShortcut : public ShortcutHandler
          */
         void useShortcut(int index)
         {
-            if (index > -1 && index < SHORTCUTS)
+            if (index > -1 && index < SHORTCUTS && mIndex[index] != -1)
                 player_node->emote(mIndex[index]);
         }
 };
