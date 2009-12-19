mana/mana

Copy ints by memcpy to avoid segfaults on sparc

src/net/ea/messageout.cpp
@@ -48,7 +48,8 @@ void MessageOut::writeInt16(Sint16 value)
 {
     expand(2);
 #if SDL_BYTEORDER == SDL_BIG_ENDIAN
-    (*(Sint16 *)(mData + mPos)) = SDL_Swap16(value);
+    Sint16 swap=SDL_Swap16(value);
+    memcpy(mData + mPos, &swap, sizeof(Sint16));
 #else
     (*(Sint16 *)(mData + mPos)) = value;
 #endif
@@ -59,7 +60,8 @@ void MessageOut::writeInt32(Sint32 value)
 {
     expand(4);
 #if SDL_BYTEORDER == SDL_BIG_ENDIAN
-    (*(Sint32 *)(mData + mPos)) = SDL_Swap32(value);
+    Sint32 swap=SDL_Swap32(value);
+    memcpy(mData + mPos, &swap, sizeof(Sint32));
 #else
     (*(Sint32 *)(mData + mPos)) = value;
 #endif

Tametomo/Aethyra

src/eathena/net/messageout.cpp
@@ -47,7 +47,8 @@ void MessageOut::writeInt8(Sint8 value)
 void MessageOut::writeInt16(Sint16 value)
 {
 #if SDL_BYTEORDER == SDL_BIG_ENDIAN
-    (*(Sint16 *)(mData + mPos)) = SDL_Swap16(value);
+    Sint16 swap = SDL_Swap16(value);
+    memcpy(mData + mPos, &swap, sizeof(Sint16));
 #else
     (*(Sint16 *)(mData + mPos)) = value;
 #endif
@@ -58,7 +59,8 @@ void MessageOut::writeInt16(Sint16 value)
 void MessageOut::writeInt32(Sint32 value)
 {
 #if SDL_BYTEORDER == SDL_BIG_ENDIAN
-    (*(Sint32 *)(mData + mPos)) = SDL_Swap32(value);
+    Sint32 swap = SDL_Swap32(value);
+    memcpy(mData + mPos, &swap, sizeof(Sint32));
 #else
     (*(Sint32 *)(mData + mPos)) = value;
 #endif