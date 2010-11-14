Tametomo/Aethyra

commit ebc40ed1f33d26f96637c57f9c7c7d61d19e6fbe

De-hardcode tile width and tile height values from within the client code, as well as a few others based off of the player sprite as well.
This could have been done a long time ago, but didn't realize that
it would be this simple. ;)

Signed-off-by: Tametomo <irarice@gmail.com>

src/core/map/sprite/flooritem.cpp
@@ -67,5 +67,6 @@ Item* FloorItem::getItem() const
 void FloorItem::draw(Graphics *graphics, const int offsetX,
                      const int offsetY) const
 {
-    graphics->drawImage(mItem->getImage(), mX * 32 + offsetX, mY * 32 + offsetY);
+    graphics->drawImage(mItem->getImage(), mX * mMap->getWidth() + offsetX,
+                        mY * mMap->getHeight() + offsetY);
 }

src/bindings/guichan/inputmanager.cpp
@@ -38,6 +38,8 @@
 
 #include "../../core/log.h"
 
+#include "../../core/map/map.h"
+
 #include "../../core/map/sprite/localplayer.h"
 
 #include "../../core/utils/dtor.h"
@@ -405,9 +407,11 @@ bool InputManager::handleKeyboardInput(const SDL_Event &event)
 
                     if (target)
                     {
-                        viewport->showPopup(target->mX * 32 -
-                                            viewport->getCameraX() + 16,
-                                            target->mY * 32 -
+                        Map *map = viewport->getMap();
+                        viewport->showPopup(target->mX * map->getTileWidth() -
+                                            viewport->getCameraX() +
+                                           (map->getTileWidth() / 2),
+                                            target->mY * map->getTileHeight() -
                                             viewport->getCameraY(), target);
                     }
                     used = true;

src/bindings/guichan/widgets/beingbox.cpp
@@ -121,7 +121,7 @@ void BeingBox::draw(gcn::Graphics *graphics)
         // Draw being
         int x, y, bs;
         bs = getFrameSize();
-        x = getWidth() / 2 - 16 + bs;
+        x = (getWidth() - (mBeing->getWidth() / 2)) / 2 + bs;
         y = getHeight() / 2 + bs;
         for (int i = 0; i < Being::VECTOREND_SPRITE; i++)
         {

src/core/map/map.cpp
@@ -104,9 +104,11 @@ void TileAnimation::update(const int ticks)
 }
 
 MapLayer::MapLayer(const int x, const int y, const int width, const int height,
+                   const int tileWidth, const int tileHeight,
                    const bool isFringeLayer, const bool isVisible):
     mX(x), mY(y),
     mWidth(width), mHeight(height),
+    mTileWidth(tileWidth), mTileHeight(tileHeight),
     mIsFringeLayer(isFringeLayer),
     mIsVisible(isVisible)
 {
@@ -158,7 +160,8 @@ void MapLayer::draw(Graphics *graphics, int startX, int startY,
         // tiles have been drawn
         if (mIsFringeLayer)
         {
-            while (si != sprites.end() && (*si)->getPixelY() <= y * 32 - 32)
+            while (si != sprites.end() &&
+                  (*si)->getPixelY() <= y * mTileHeight - mTileHeight)
             {
                 (*si)->draw(graphics, -scrollX, -scrollY);
                 si++;
@@ -170,8 +173,9 @@ void MapLayer::draw(Graphics *graphics, int startX, int startY,
             Image *img = getTile(x, y);
             if (img)
             {
-                const int px = (x + mX) * 32 - scrollX;
-                const int py = (y + mY) * 32 - scrollY + 32 - img->getHeight();
+                const int px = (x + mX) * mTileWidth - scrollX;
+                const int py = (y + mY) * mTileHeight - scrollY + mTileHeight -
+                               img->getHeight();
                 graphics->drawImage(img, px, py);
             }
         }

src/core/map/map.h
@@ -98,6 +98,7 @@ class MapLayer
          * There can be only one fringe layer per map.
          */
         MapLayer(const int x, const int y, const int width, const int height,
+                 const int tileWidth, const int tileHeight,
                  const bool isFringeLayer, const bool isVisible = true);
 
         /**
@@ -135,6 +136,7 @@ class MapLayer
     private:
         int mX, mY;
         int mWidth, mHeight;
+        int mTileWidth, mTileHeight;
         bool mIsFringeLayer;    /**< Whether the sprites are drawn. */
         bool mIsVisible;
         Image **mTiles;

src/core/map/mapreader.cpp
@@ -221,6 +221,8 @@ void MapReader::readLayer(const xmlNodePtr &node, Map *map)
     // Layers are not necessarily the same size as the map
     const int w = XML::getProperty(node, "width", map->getWidth());
     const int h = XML::getProperty(node, "height", map->getHeight());
+    const int tw = XML::getProperty(node, "tilewidth", map->getTileWidth());
+    const int th = XML::getProperty(node, "tileheight", map->getTileHeight());
     const int offsetX = XML::getProperty(node, "x", 0);
     const int offsetY = XML::getProperty(node, "y", 0);
     std::string name = XML::getProperty(node, "name", "");
@@ -234,7 +236,8 @@ void MapReader::readLayer(const xmlNodePtr &node, Map *map)
 
     if (!isCollisionLayer)
     {
-        layer = new MapLayer(offsetX, offsetY, w, h, isFringeLayer, isVisible);
+        layer = new MapLayer(offsetX, offsetY, w, h, tw, th, isFringeLayer,
+                             isVisible);
         map->addLayer(layer);
     }
 

src/core/map/sprite/being.cpp
@@ -65,12 +65,6 @@
 
 int Being::mNumberOfHairstyles = 1;
 
-static const int X_SPEECH_OFFSET = 18;
-static const int Y_SPEECH_OFFSET = 60;
-
-static const int DEFAULT_WIDTH = 32;
-static const int DEFAULT_HEIGHT = 32;
-
 Being::Being(const int id, const int job, Map *map):
     mJob(job),
     mX(0), mY(0),
@@ -419,8 +413,8 @@ void Being::logic()
     int oldPy = mPy;
 
     // Update pixel coordinates
-    mPx = mX * 32 + getXOffset();
-    mPy = mY * 32 + getYOffset();
+    mPx = mX * mMap->getTileWidth() + getXOffset();
+    mPy = mY * mMap->getTileHeight() + getYOffset();
 
     if (mPx != oldPx || mPy != oldPy)
         updateCoords();
@@ -443,7 +437,8 @@ void Being::logic()
     }
 
     // Update particle effects
-    mChildParticleEffects.moveTo((float) mPx + 16.0f, (float) mPy + 32.0f);
+    mChildParticleEffects.moveTo((float) mPx + ((float) mMap->getTileWidth() / 2),
+                                 (float) mPy + ((float) mMap->getTileHeight()));
 }
 
 void Being::draw(Graphics *graphics, const int offsetX,
@@ -469,7 +464,7 @@ void Being::drawEmotion(Graphics *graphics, const int offsetX,
         return;
 
     const int px = mPx - offsetX;
-    const int py = mPy - offsetY - 64;
+    const int py = mPy - offsetY - getHeight();
     const int emotionIndex = mEmotion - 1;
     const AnimatedSprite *sprite = EmoteDB::getAnimation(emotionIndex);
 
@@ -492,8 +487,8 @@ void Being::drawSpeech(const int offsetX, const int offsetY)
 
         const bool hasCaption = (mSpeechBubble->getCaption() != "");
         const bool showName = (speech == NAME_IN_BUBBLE);
-        const int width = 16;
-        const int height = getHeight() - 32;
+        const int width = mMap->getTileWidth() / 2;
+        const int height = getHeight() - mMap->getTileHeight();
 
         if (mOldSpeech != mSpeech || mText)
         {
@@ -517,7 +512,7 @@ void Being::drawSpeech(const int offsetX, const int offsetY)
     else if (mSpeechTime > 0 && speech == TEXT_OVERHEAD)
     {
         if (mSpeech == mOldSpeech && mText)
-            mText->adviseXY(mPx + X_SPEECH_OFFSET, mPy - Y_SPEECH_OFFSET);
+            mText->adviseXY(mPx + ((getHeight() + 4) / 2), mPy - getHeight() - 4);
         else
         {
             // don't introduce a memory leak
@@ -528,8 +523,8 @@ void Being::drawSpeech(const int offsetX, const int offsetY)
 
             mOldSpeech = mSpeech;
 
-            mText = new Text(mSpeech, mPx + X_SPEECH_OFFSET, mPy - Y_SPEECH_OFFSET,
-                             gcn::Graphics::CENTER,
+            mText = new Text(mSpeech, mPx + ((getHeight() + 4) / 2), mPy -
+                             getHeight() - 4, gcn::Graphics::CENTER,
                              &guiPalette->getColor(Palette::PARTICLE));
         }
     }
@@ -556,21 +551,43 @@ void Being::setWalkSpeed(const Uint16 &speed)
     mWalkSpeed = speed > 0 ? speed <= 1000 ? speed : 1000 : 1;
 }
 
-const int Being::getOffset(const char &pos, const char &neg) const
+const int Being::getXOffset() const
 {
     // Check whether we're walking in the requested direction
-    if (mAction != WALK || !(mDirection & (pos | neg)))
+    if (mAction != WALK || !(mDirection & (LEFT | RIGHT)))
         return 0;
 
-    int offset = (get_elapsed_time(mWalkTime) * 32) / mWalkSpeed;
+    int offset = (get_elapsed_time(mWalkTime) * mMap->getTileWidth()) /
+                 mWalkSpeed;
 
     // We calculate the offset _from_ the _target_ location
-    offset -= 32;
+    offset -= mMap->getTileWidth();
     if (offset > 0)
         offset = 0;
 
     // Going into negative direction? Invert the offset.
-    if (mDirection & pos)
+    if (mDirection & LEFT)
+        offset = -offset;
+
+    return offset;
+}
+
+const int Being::getYOffset() const
+{
+    // Check whether we're walking in the requested direction
+    if (mAction != WALK || !(mDirection & (UP | DOWN)))
+        return 0;
+
+    int offset = (get_elapsed_time(mWalkTime) * mMap->getTileHeight()) /
+                 mWalkSpeed;
+
+    // We calculate the offset _from_ the _target_ location
+    offset -= mMap->getTileHeight();
+    if (offset > 0)
+        offset = 0;
+
+    // Going into negative direction? Invert the offset.
+    if (mDirection & UP)
         offset = -offset;
 
     return offset;
@@ -578,18 +595,30 @@ const int Being::getOffset(const char &pos, const char &neg) const
 
 const int Being::getWidth() const
 {
-    if (AnimatedSprite *base = mSprites[BASE_SPRITE])
-        return std::max(base->getWidth(), DEFAULT_WIDTH);
+    AnimatedSprite *base = mSprites[BASE_SPRITE];
+
+    if (mMap && base)
+        return std::max(base->getWidth(), mMap->getTileWidth());
+    else if (mMap)
+        return mMap->getTileWidth();
+    else if (!mMap && base)
+        return std::max(base->getWidth(), 1);
     else
-        return DEFAULT_WIDTH;
+        return 1;
 }
 
 const int Being::getHeight() const
 {
-    if (AnimatedSprite *base = mSprites[BASE_SPRITE])
-        return std::max(base->getHeight(), DEFAULT_HEIGHT);
+    AnimatedSprite *base = mSprites[BASE_SPRITE];
+
+    if (mMap && base)
+        return std::max(base->getHeight(), mMap->getTileHeight());
+    else if (mMap)
+        return mMap->getTileHeight();
+    else if (!mMap && base)
+        return std::max(base->getHeight(), 1);
     else
-        return DEFAULT_HEIGHT;
+        return 1;
 }
 
 void Being::setTargetAnimation(SimpleAnimation* animation)

src/core/map/sprite/being.h
@@ -136,7 +136,13 @@ class Being : public Sprite
         /**
          * Directions, to be used as bitmask values
          */
-        enum { DOWN = 1, LEFT = 2, UP = 4, RIGHT = 8 };
+        enum Direction
+        {
+            DOWN = 1,
+            LEFT = 2,
+            UP = 4,
+            RIGHT = 8
+        };
 
         Uint16 mJob;          /**< Job (player job, npc, monster, ) */
         Uint16 mX, mY;        /**< Tile coordinates */
@@ -350,12 +356,12 @@ class Being : public Sprite
         /**
          * Get the current X pixel offset.
          */
-        const int getXOffset() const { return getOffset(LEFT, RIGHT); }
+        const int getXOffset() const;
 
         /**
          * Get the current Y pixel offset.
          */
-        const int getYOffset() const { return getOffset(UP, DOWN); }
+        const int getYOffset() const;
 
         /**
          * Returns the horizontal size of the current base sprite of the being
@@ -440,11 +446,6 @@ class Being : public Sprite
         ParticleList mChildParticleEffects;
 
     private:
-        /**
-         * Calculates the offset in the given directions.
-         * If walking in direction 'neg' the value is negated.
-         */
-        const int getOffset(const char &pos, const char &neg) const;
 
         // Speech Bubble components
         SpeechBubble *mSpeechBubble;

commit 1f69bf9210292228d25b991238a98a86503fc639

Fix display of floor items again, which was accidentally broken in ebc40ed1. The mMap->getWidth() and mMap->getHeight() instances were supposed to be mMap->getTileWidth() and mMap->getTileHeight() respectively.
Bug noticed by Octalot

Signed-off-by: Tametomo <irarice@gmail.com>

src/core/map/sprite/flooritem.cpp
@@ -65,6 +65,6 @@ Item* FloorItem::getItem() const
 void FloorItem::draw(Graphics *graphics, const int offsetX,
                      const int offsetY) const
 {
-    graphics->drawImage(mItem->getImage(), mX * mMap->getWidth() + offsetX,
-                        mY * mMap->getHeight() + offsetY);
+    graphics->drawImage(mItem->getImage(), mX * mMap->getTileWidth() + offsetX,
+                        mY * mMap->getTileHeight() + offsetY);
 }