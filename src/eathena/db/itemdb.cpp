6c4a5aced4194c305c4dc0122d3e34e4dd404eb5
Tametomo <irarice@gmail.com>
2011-04-02 14:46:54 -0600

    Get rid of some unnecessary asserts in ItemDB. If we detect that the item database isn't loaded, the proper way to solve this is to actually load it, not complain that it wasn't loaded.
    Signed-off-by: Tametomo <irarice@gmail.com>

--- src/eathena/db/itemdb.cpp
+++ src/eathena/db/itemdb.cpp
@@ -20,8 +20,6 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#include <cassert>
-
 #include <libxml/tree.h>
 
 #include "itemdb.h"
@@ -172,7 +170,8 @@ void ItemDB::unload()
 
 const ItemInfo& ItemDB::get(const int id)
 {
-    assert(mLoaded);
+    if (!mLoaded)
+        load();
 
     const ItemInfoIterator i = mItemInfos.find(id);
 
@@ -187,7 +186,8 @@ const ItemInfo& ItemDB::get(const int id)
 
 const ItemInfo& ItemDB::get(const std::string &name)
 {
-    assert(mLoaded);
+    if (!mLoaded)
+        load();
 
     std::string item = name;
     const NamedItemInfoIterator i = mNamedItemInfos.find(normalize(item));
