d703b304112b712d2877da31d0b0281be082a964
Thorbjørn Lindeijer <thorbjorn@lindeijer.nl>
2012-07-22 21:12:15 +0200

    Fixed mixup of parameter types in OpenGL code
    Textures were not rendering correctly on cards that did not support the
    GL_ARB_texture_rectangle extension, since the alternative code path was
    passing ints as floats and floats as ints to OpenGL.

--- src/bindings/guichan/opengl/openglgraphics.cpp
+++ src/bindings/guichan/opengl/openglgraphics.cpp
@@ -147,8 +147,8 @@ static inline void drawQuad(Image *image, int srcX, int srcY, int dstX,
         GLint vert[] = { dstX, dstY, dstX + width, dstY,
                          dstX + width, dstY + height, dstX, dstY + height };
 
-        glVertexPointer(2, GL_FLOAT, 0, &vert);
-        glTexCoordPointer(2, GL_INT, 0, &tex);
+        glVertexPointer(2, GL_INT, 0, &vert);
+        glTexCoordPointer(2, GL_FLOAT, 0, &tex);
         glDrawArrays(GL_QUADS, 0, 4);
     }
     else
